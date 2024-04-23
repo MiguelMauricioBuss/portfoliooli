1b0a998f315306debb8959dbe8867fa38d974d91
MiguelMauricioBuss <125047226+MiguelMauricioBuss@users.noreply.github.com>
2024-04-22 21:05:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function linkd(){
+    window.open("https://www.linkedin.com/in/miguel-mauricio-buss-841469257/")
+}
+function insta(){
+    window.open("https://www.instagram.com/olliveira_dudu/")
+}
+function github(){
+    window.open("https://github.com/edua4rdo0liveira77")
+}
